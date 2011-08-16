94ede7a9e10bb3deb3c50daa31c8cfbac4b15ea5
Cedric <cedric at code.tl>
2011-08-16 01:14:09 -0600

    Licensing

--- www/index.php
+++ www/index.php
@@ -1,4 +1,20 @@
 <?php
+/*
+This file is part of the status project.
+
+    The status project is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The status project is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with the status project.  If not, see <http://www.gnu.org/licenses/>.
+*/
 	require('../config.php');
 	$start = microtime();
 	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
