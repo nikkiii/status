6a69e34e18e9eb843f7767db24d14786f0cf04a6
cedricd <github-com@a.insertmail.com>
2011-08-17 04:40:40 +0000

    "Fork me on GitHub" banner (from GitHub's blog)

--- www/index.php
+++ www/index.php
@@ -33,6 +33,7 @@ This file is part of the status project.
 		</style>
 	</head>
 	<body>
+		<a href="https://github.com/cedricd/status"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub"></a>
 		<div id="wrapper">
 			<h1>Server statistics</h1>
 			<div class="stats_container" id="stats">
