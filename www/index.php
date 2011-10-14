b3d0da3a1b9f0893becfdde2973c780a0c033507
Nikki <nikki@nikkii.us>
2011-10-14 16:54:07 -0400

    Validation fix

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@ This file is part of the status project.
     You should have received a copy of the GNU Affero General Public License
     along with the status project.  If not, see <http://www.gnu.org/licenses/>.
 */
-	require('../config.php');
+	require('/usr/local/servers/config.php');
 	$start = microtime();
 	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 	header('Cache-Control: no-store, no-cache, must-revalidate');
@@ -23,17 +23,17 @@ This file is part of the status project.
 	header('Pragma: no-cache');
 	header('Refresh: 60');
 ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
 	<head>
-		<meta charset="utf-8">
 		<title>Server statistics</title>
 		<style type="text/css">
 			body{font-family:Helvetica, Verdana;}h1{font-size:32px;}div#wrapper{width:95%;margin-left:auto;margin-right:auto;position:relative;text-align:center;}div.stats_container{position:relative;text-align:center;}div.clear{clear:both;font-size:small;}div.stats_status{font-size:12px;color:red;margin-top:2px;margin-bottom:10px;}table,td{border:1px solid #CCC;font-size:11.5px;margin:0 auto;}thead th,tbody th{background:url(data:image/gif;base64,R0lGODlhAQAfALMAAPLz9fn6/fv8/vX1+Pf5++/w8e3u7+rq6+rr7AAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAAAAAALAAAAAABAB8AAAQQ8MiDqjElgz06+UEYCGQZAQA7) repeat-x;color:#666;border:1px solid #CCC;padding:5px 10px;}tfoot td,tfoot th{border:1px solid #CCC;color:#666;padding:4px;}div.progress-container{border:1px solid #ccc;width:100px;text-align:left;background:#FFF;text-size:3px;float:left;margin:2px 5px 2px 0;padding:1px;}div.progress-container-percent{padding:2px;}div.progress-container > div{background-color:/*#ACE97C*/#00BFFF;height:12px;}.offline{background-color:#FFE6E6;}.online-but-no-data{background-color:#3F6;}.5pad{padding:5px;}.bartext{text-align:center;width:100px;}.loadavg{-moz-border-radius:5px;border-radius:5px;text-align:center;padding:2px;}a:link,a:active,a:visited{color:#666;text-decoration:underline;}
 		</style>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	</head>
 	<body>
-		<a href="https://github.com/nikkiii/status"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub"></a>
+		<a href="https://github.com/nikkiii/status"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/30f550e0d38ceb6ef5b81500c64d970b7fb0f028/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" /></a>
 		<div id="wrapper">
 			<h1>Server statistics</h1>
 			<div class="stats_container" id="stats">
@@ -51,8 +51,7 @@ This file is part of the status project.
 				<th scope="col">Load</th>
 			</tr>
 		</thead>
-			<tfoot>
-
+		<tbody>
 <?php
 	try {
 		$db = new PDO('sqlite:'. $db);
@@ -135,7 +134,7 @@ This file is part of the status project.
 	}
 	$end = microtime();
 ?>
-			</tfoot>
+			</tbody>
 	</table>
 				</div>
 			</div>
